66e050402de91b6db15b496eb0073c06e7a0647a
armaan-mann <armaan.mann@mail.utoronto.ca>
2021-03-30 13:54:58 -0400

    Update extracting_reviews.py

--- extracting_reviews.py
+++ extracting_reviews.py
@@ -1,7 +1,8 @@
 """
 Final Project Title: ...
 
-Objective: ...
+Objective: Fetch the reviews from the internet and coverting it into a CSV file such that
+we can use it for the UI
 
 By: Ansh Malhotra, Armaan Mann, Leya Abubaker, Gabriel Pais
 
