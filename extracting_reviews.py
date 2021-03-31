0459e4fad7b5e7ad9c6eae22a40a6386355b0ab5
armaan-mann <armaan.mann@mail.utoronto.ca>
2021-03-30 23:29:04 -0400

    Update extracting_reviews.py

--- extracting_reviews.py
+++ extracting_reviews.py
@@ -8,3 +8,81 @@ By: Ansh Malhotra, Armaan Mann, Leya Abubaker, Gabriel Pais
 
 This file is Copyright (c) 2020 Ansh Malhotra, Armaan Mann, Leya Abubaker, Gabriel Pais
 """
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import pprint
+
+
+def fetch_movie_reviews(review_title: str) -> dict:
+    """
+    Return a dictionary, where the key is the title of the review and the value
+    is the description corresponding to the title. Takes in a set of titles and returns
+    the reviews for those
+    """
+
+    # First we need to set up the browser
+    browser = webdriver.Chrome(executable_path=r'C:\Users\Armaa\Music\UOFT '
+                                               r'CSC111\csc111\chromedriver.exe')
+    # options = Options()
+    # options.add_argument("--headless")
+    # browser = webdriver.Chrome(
+    #     executable_path=r'C:\Users\Armaa\Music\UOFT CSC111\csc111\chromedriver.exe',
+    #     options=options)
+
+    # Go to google the Browser
+    browser.get('https://www.google.com')
+
+    # Enters movie name + imbd in the google search bar:
+    browser.find_element_by_name('q').send_keys(review_title + " imdb")
+    time.sleep(1)
+
+    # # Clicks the "Search" on google to see the results
+    # browser.find_element_by_name("btnK").send_keys(Keys.ENTER)
+    # time.sleep(1)
+    #
+    # # Clicks on the first search result (IMBD for the movie name)
+    # browser.find_element_by_name("yuRUbf").click()
+    # time.sleep(5)
+    #
+    # # Clicks the "User Reviews" opn the movie IMBD page
+    # browser.find_element_by_xpath(
+    #     "//*[@id='quicklinksMainSection']/a[3]").click()
+
+    # Click the google search button
+    browser.find_element_by_name("btnK").send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    # Click the link
+    browser.find_element_by_xpath("//*[@id='rso']/div[1]/div/div/div/div[1]/a").click()
+    time.sleep(5)
+
+    # Click the user reviews
+    browser.find_element_by_xpath(
+        "//*[@id='quicklinksMainSection']/a[3]").click()
+
+    # Fetch the IMBD reviews
+    contain_url = browser.current_url
+    link_request = requests.get(contain_url)
+    extract_html = BeautifulSoup(link_request.content, "html.parser")
+    search = extract_html.find(id="main")
+
+    # Get the title of the movie
+    element1 = search.find(class_="parent")
+    element2 = element1.find(itemprop="name")
+    element3 = element2.find(itemprop='url')
+
+    # Review Title Extraction
+    extract_review_title = search.select(".title")
+    review_title = [raw_title.get_text().replace("\n", "") for raw_title in extract_review_title]
+
+    # Review Description Title
+    extarct_review_description = search.select(".content .text")
+    review_description = \
+        [raw_review_description.get_text() for raw_review_description in extarct_review_description]
+
+    return {"Review Title": review_title,
+            "Review Description": review_description}
