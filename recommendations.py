701168458ea38db70df7a513c31bd38f21239ace
Ansh757 <67304812+Ansh757@users.noreply.github.com>
2021-04-10 01:05:37 -0400

    Delete recommendations.py

--- recommendations.py
+++
@@ -1,9 +0,0 @@
-"""
-Final Project Title: ...
-
-Objective: ...
-
-By: Ansh Malhotra, Armaan Mann, Leya Abubaker, Gabriel Pais
-
-This file is Copyright (c) 2020 Ansh Malhotra, Armaan Mann, Leya Abubaker, Gabriel Pais
-"""
